16fc645d48b0b78fa58b7d086228cd7b901e9504
Koske Kimutai <koskedk@live.com>
2020-03-10 10:28:43 +0300

    ver updates

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'LiveSYNC running...';
+    return 'LiveSYNC running [v05MAR202209]...';
   }
 }
