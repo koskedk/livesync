56e7ccd12911263aa731f865aa648f293d8d4ad1
Koske Kimutai <koskedk@live.com>
2021-05-06 03:02:21 +0300

    updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   apps : [{
     name: 'livesync',
     script: 'dist/main.js',
@@ -13,21 +12,4 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }]
-=======
-  apps: [
-    {
-      name: 'livesync',
-      script: 'dist/main.js',
-      instances: 1,
-      autorestart: true,
-      watch: true,
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
->>>>>>> 6b1ecba60075d1f03c797823146b90d1390bc90f
 };
